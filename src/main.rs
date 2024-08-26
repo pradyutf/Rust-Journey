e437062d27d3e03be5a0193d279334e8086b665c
pradyutf <pradyutfogla@gmail.com>
2024-08-26 11:19:13 +0530

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
 fn main() {
     println!("Hello, world!");
+
+    let x = 34;
+    let y: u32 = 4;
+    let z: f32 = 23.0;
+
+
+
+    
+
+    println!("x={}, y={}, z={}",x,y,z);
+
+    let greet = "fvoemv";
+
+
+    let greeting = String::from("Hello World");
+    println!("{}",greeting);
+
+    fn get_first_word(sentence: String)-> String {
+        let mut ans = String::from("yupp");
+        return ans;
+    }
+
+    let ree = get_first_word(greeting);
+    println!("{}",ree);
+
+
+
 }
+
+
+
+
